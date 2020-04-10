173854830513c2e838cca89d3d269adbb534e886
hoda <hadenhiles@gmail.com>
2020-04-10 14:52:51 -0400

    Add custom merch page

---
+++ merch/index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Merch | How To Hockey</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/responsive.css" />
+    <link rel="stylesheet" href="./css/fonts.css" />
+    <link rel="stylesheet" href="./css/custom.css" />
+
+    <!-- FAVICON -->
+    <link rel="apple-touch-icon" sizes="57x57" href="./images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./images/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./images/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+</head>
+
+<body class="template-index scheme_1 ">
+    <div class="page_wrapper">
+        <div class="page_container">
+            <div class="main_content">
+                <div id="shopify-section-1568977949738" class="shopify-section section section_homepage section_banners">
+                    <div class="row">
+                        <div class="col-sm-12 banner_100">
+                            <div class="banner_item">
+                                <img style="width: 100%" class="banner_image" src="./images/banner.png">
+                                <div class="banner_caption style_telling">
+                                    <div class="caption_text">
+                                        <img src="./images/logo-white.png" class="logo" />
+                                        <h2>#TeamHTH</h2>
+                                        <h4>MERCH</h4>
+                                        <!-- <a class="btn" href="#">Shop Merch</a> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="shopify-section-1566390937705" class="shopify-section section section_homepage section_banners collections">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 banner_25">
+                            <div class="banner_item hover_image">
+
+                                <img style="width: 100%" class="banner_image" src="./images/classics.png" alt="" data-src="./images/classics.png" data-widths="[360, 540, 720, 900, 1080, 1296, 1512, 1728, 1810]">
+
+                                <div class="banner_caption style_minimal">
+
+                                    <a class="caption_text" href="https://www.pacificrink.com/collections/how-to-hockey">
+
+                                        <p class="banner_text">The Classics</p>
+
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 banner_25">
+                            <div class="banner_item hover_image">
+
+                                <img style="width: 100%" class="banner_image" src="./images/teespring.png" alt="" data-src="./images/teespring.png" data-widths="[360, 540, 720, 900, 1080, 1296, 1512, 1728, 1810]">
+
+                                <div class="banner_caption style_minimal">
+
+                                    <a class="caption_text " href="https://teespring.com/stores/howtohockey">
+
+                                        <p class="banner_text">Latest</p>
+
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 banner_25">
+                            <div class="banner_item hover_image">
+
+                                <img style="width: 100%" class="banner_image" src="./images/jerseys.png" alt="" data-src="./images/jerseys.png" data-widths="[360, 540, 720, 900, 1080, 1296, 1512, 1728, 1810]">
+
+                                <div class="banner_caption style_minimal">
+
+                                    <a class="caption_text" href="https://mensleaguesweaters.com/products/authentic-custom-how-to-hockey-jersey">
+
+                                        <p class="banner_text">Jerseys</p>
+
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 banner_25">
+                            <div class="banner_item hover_image">
+
+                                <img style="width: 100%" class="banner_image" src="./images/summer-skates.png" alt="" data-src="./images/summer-skates.png" data-widths="[360, 540, 720, 900, 1080, 1296, 1512, 1728, 1810]">
+
+                                <div class="banner_caption style_minimal">
+
+                                    <a class="caption_text " href="https://www.summerskates.ca/products/how-to-hockey">
+
+                                        <p class="banner_text">Summer Skates</p>
+
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div id="shopify-section-1569070987321" class="shopify-section section section_newsletter">
+                    <div class="index_newsletter_wrapper">
+                        <div class="container">
+                            <div class="item__newsletter">
+                                <h4>Subscribe to our hockey newsletter</h4>
+                                <form action="https://app.getresponse.com/add_subscriber.html" accept-charset="utf-8" method="post">
+                                    <p class="alert alert-success hidden">You have successfully subscribed!</p>
+                                    <p class="form_text">You may unsubscribe at any time.</p>
+                                    <input type="hidden" name="contact[tags]" value="Newsletter subscriber">
+                                    <div class="form_wrapper">
+                                        <input type="email" name="email" class="input-group__field newsletter__input" placeholder="Email address">
+                                        <button class="btn" type="submit">Subscribe</button>
+                                    </div>
+                                    <input type="hidden" name="campaign_token" value="npaD" />
+                                    <input type="hidden" name="start_day" value="0" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div id="shopify-section-1572613306853" class="shopify-section section section_divider">
+                    <div class="divider_item container" style=""></div>
+
+                    <div class="divider_item tablet_show container" style="margin-top: 1px;"></div>
+
+                </div>
+                <!-- END content_for_index -->
+
+            </div>
+
+            <div id="shopify-section-footer-1" class="shopify-section">
+                <footer class="footer_type_1">
+                    <div class="footer_row__2">
+                        <div class="footer_bottom_pannel container_big">
+                            <div class="row">
+
+                                <div class="footer_wrap_2 footer_copyright">
+                                    <a href="http://howtohockey.com"><h6>How To Hockey</h6></a><i>|</i> © 2020
+                                </div>
+
+                                <ul class="social_menu footer_wrap_2">
+
+                                    <li><a href="https://www.facebook.com/howtohockey/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="https://twitter.com/howtohockey"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="https://www.instagram.com/howtohockey"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="https://www.youtube.com/coachjeremy"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </footer>
+
+            </div>
+
+        </div>
+
+        <a id="back_top" href="#">
+            <i class="fa fa-angle-up" aria-hidden="true"></i>
+        </a>
+
+    </div>
+
+</body>
+
+</html>
