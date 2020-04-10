c476a2161ff9092d368fdba989b736d3ec899e35
hoda <hadenhiles@gmail.com>
2020-04-10 17:20:23 -0400

    Resolves #1. Remove unwanted success message url params after page loads.

--- merch/index.php
+++ merch/index.php
@@ -209,8 +209,8 @@
 
     </div>
 
-    <script type="text/text/javascript" src="./js/main.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
 </body>
 
 </html>
--- merch/js/main.js
+++ merch/js/main.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-  
+  history.replaceState && history.replaceState(
+    null, '', location.pathname + location.search.replace(/[\?&]subscribed=[^&]+/, '').replace(/^&/, '?').replace(/[\#&]newsletter=[^&]+/, '').replace(/^&/, '#')
+  );
 });
